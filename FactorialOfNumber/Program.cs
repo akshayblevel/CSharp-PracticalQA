3ef57c67386d4d40a2a57b01acb77ef26bd6a122
akshayblevel <akshayblevel@hotmail.com>
2018-10-12 00:23:42 +0530

    First Commit

---
+++ FactorialOfNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER THE NUMBER");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+                Console.WriteLine("INVALID INPUT");
+            else if (num == 0)
+                Console.WriteLine("FACTORIAL= 1");
+            else
+                Fact(num);
+
+            Console.ReadLine();
+        }
+
+        public static void Fact(int num)
+        {
+            int i, factorial = 1;
+
+            for (i = 1; i <= num; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("FACTORIAL= " + factorial);
+        }
+    }
+}
---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNo = 0, secondNo = 1;
+            Console.WriteLine("ENTER FIBONACCI SERIES OF NTH TERM: ");
+             int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("First Number={0},Second Number={1}", firstNo, secondNo);
+            Fib(n);
+            Console.ReadLine();
+        }
+
+        public static void Fib(int n)
+        {
+            int i, currentNo, firstNo = 0, secondNo = 1;
+            for (i = 0; i <= (n - 3); i++)
+            {
+                currentNo = firstNo + secondNo;
+                firstNo = secondNo;
+                secondNo = currentNo;
+                Console.WriteLine(currentNo);
+            }
+        }
+    }
+}
