a17fe6d83abf8dbe8aff10dd7afbbfa3066c0add
akshayblevel <akshayblevel@hotmail.com>
2018-10-12 12:50:19 +0530

    Remove Duplicate Character From String

---
+++ RemoveDuplicateCharFromString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicateCharFromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String To Remove Duplicate Character:");
+            string str = Console.ReadLine();
+
+
+            string duplicate = string.Empty;
+            string result = "";
+            foreach (char value in str)
+            {
+                if (duplicate.IndexOf(value) == -1)
+                {
+                    duplicate += value;
+                    result += value;
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
